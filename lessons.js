bc7e38a9e2fa3bbf4870733a2739652d00b3331c
Dhrumin <dp880@live.mdx.ac.uk>
2021-11-19 21:42:24 +0000

    Created new folder and saved pictures

---
+++ lessons.js
@@ -0,0 +1,103 @@
+//Vue Object creation
+let lessons = [
+
+    {
+        id: 1,
+        subject: "Math" ,
+        location: "Luton" ,
+        price: 100,
+        image: "image/maths.png", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 2,
+        subject: "English" ,
+        location: "Bath" ,
+        price: 80,
+        image: "image/english.jpg",
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 3,
+        subject: "Music" ,
+        location: "Yorkshire" ,
+        price: 100,
+        image: "image/Music.png", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 4,
+        subject: "Law" ,
+        location: "Manchester" ,
+        price: 200,
+        image: "image/Law.jpg", 
+ 	availableInventory: 5
+        
+    },
+    
+    {
+        id: 5,
+        subject: "History" ,
+        location: "Watford" ,
+        price: 100,
+        image: "image/History.jpg", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 6,
+        subject: "Craft" ,
+        location: "Briton" ,
+        price: 75,
+        image: "image/Craft.jpg", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 7,
+        subject: "Economics" ,
+        location: "Northamton" ,
+        price: 150,
+        image: "image/Economics.jpg", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 8,
+        subject: "Biology" ,
+        location: "Liverpool" ,
+        price: 300,
+        image: "image/Biology.jpg", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 9,
+        subject: "Physical Education",
+        location: "Leicester" ,
+        price: 100,
+        image: "image/PE.jpg", 
+        availableInventory: 5
+        
+    },
+    
+    {
+        id: 10,
+        subject: "Foreign Language",
+        location: "Nottingham" ,
+        price: 200,
+        image: "image/FL.jpg", 
+        availableInventory: 5
+        
+    },
+    ];
